75db71d099bb835043c9eae09d812302d0a0f483
Z-007-0 <joe-is@hotmail.co.uk>
2023-02-04 23:09:49 +0000

    Update PostProcessingVolumeManager.cs

--- Assets/Data/Scripts/Singletons/PostProcessingVolumeManager.cs
+++ Assets/Data/Scripts/Singletons/PostProcessingVolumeManager.cs
@@ -12,51 +12,53 @@ public class PostProcessingVolumeManager : SingletonParent<PostProcessingVolumeM
     [SerializeField] private  PostProcessVolume m_Heaven;
     [SerializeField] private  PostProcessVolume m_Hell;
     [Tooltip("This is a normalized value (0-1)"), Range(0, 1)] public float HeavenToHellSlider;
-
-    private float testMax = 10;
-    private float testMin = 7;
-    private float normie;
+    [SerializeField] private Transform rootsManagerXScale;
+    private float hellxScale = 0.5f;
+    private float heavenXscale = 10f;
     
-    public void SetHeavenToHellPPVolumeWeight(float value)
-    {
-        if (value > 1)
-        {
-            m_Heaven.weight = 1;
-        }
-        else if (value < 0)
-        {
-            m_Heaven.weight = 0;
-        }
-
-        m_Heaven.weight = value;
-        m_Hell.weight = 1 - value;
-
-    }
-
-    public void NSetPPWeight(float realValue, float maxValue)
-    {
-        normie = realValue / maxValue;
-        Debug.Log($"Max:{testMax}, Min:{testMin}, the normie is: {normie}");
-        
-    }
-
-
-    public void RestPPWeight(Scene arg0, LoadSceneMode loadSceneMode)
-    {
-        m_Heaven.weight = 1;
-        m_Hell.weight = 0;
-    }
     
-
     // Start is called before the first frame update
     void Start()
     {
-        NSetPPWeight(12, 20);
-        SceneManager.sceneLoaded += RestPPWeight;
+        SceneManager.sceneLoaded += ResetPPWeight;
     }
 
     private void OnDisable()
     {
-        SceneManager.sceneLoaded -= RestPPWeight;
+        SceneManager.sceneLoaded -= ResetPPWeight;
+    }
+
+    private void Update()
+    {
+        SetHeavenToHellPPVolumeWeight(rootsManagerXScale.localScale.x);
+    }
+
+    public void SetHeavenToHellPPVolumeWeight(float rootsXScale)
+    {
+        float normie;
+        normie = rootsXScale / heavenXscale;
+        
+        if (normie > 1)
+        {
+            m_Heaven.weight = 0;
+            m_Heaven.weight = 1;
+        }
+        else if (normie < 0)
+        {
+            m_Heaven.weight = 1;
+            m_Hell.weight = 0;
+        }
+
+        m_Heaven.weight = normie;
+        m_Hell.weight = 1-normie;
+        
+        Debug.Log($"Heaven: {m_Heaven.weight}, Hell: {m_Hell.weight}");
+
+    }
+    
+    public void ResetPPWeight(Scene arg0, LoadSceneMode loadSceneMode)
+    {
+        m_Heaven.weight = 1;
+        m_Hell.weight = 0;
     }
 }
